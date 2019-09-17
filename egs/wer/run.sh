09508db0b483d2eb6b62fc32f0970f87148470d9
Engine.Wang <engine.wang@liulishuo.com>
2019-09-17 12:04:17 +0800

    update

--- egs/wer/run.sh
+++ egs/wer/run.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
-#
-## cmp the best file
+
+# cmp the best file
+python ../../run_wer.py \
+    --best tmp.best \
+    --refer tmp.refer \
+    --filter ../../filters/wer_hyp_filter \
+    --special_word "<?>"
+
+
+# cmp the N-best file with score
 #python ../../run_wer.py \
-#    --best tmp.best \
+#    --nbest tmp.nbest \
+#    --score tmp.score \
 #    --refer tmp.refer \
 #    --filter ../../filters/wer_hyp_filter \
 #    --special_word "<?>" \
-#    > best.log
+#    > nbest.log
 #
-#
-# cmp the N-best file with score
-python ../../run_wer.py \
-    --nbest tmp.nbest \
-    --score tmp.score \
-    --refer tmp.refer \
-    --filter ../../filters/wer_hyp_filter \
-    --special_word "<?>" \
-    > nbest.log
-
-# cmp the oracle WER
-python ../../run_wer.py \
-    --nbest tmp.nbest \
-    --refer tmp.refer \
-    --filter ../../filters/wer_hyp_filter \
-    --special_word "<?>" \
-    --oracle \
-    > oracle-wer.log
+## cmp the oracle WER
+#python ../../run_wer.py \
+#    --nbest tmp.nbest \
+#    --refer tmp.refer \
+#    --filter ../../filters/wer_hyp_filter \
+#    --special_word "<?>" \
+#    --oracle \
+#    > oracle-wer.log
